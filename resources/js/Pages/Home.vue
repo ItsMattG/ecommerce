5908a831f551585386dd12fb0e3ef54f891fe6c3
ItsMattG <gleesonmatt96@gmail.com>
2024-03-30 09:57:35 +1100

    Some more UI layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,11 +1,10 @@
 <template>
 	<navbar></navbar>
-	<div class="main">
-		<div class="hero">
-			<div class="hero-container">
-
-				<!-- Left side: Title and Subtitle -->
-				<div class="hero-content">
+	<div class="main-container">
+		<div class="main-one">
+			<!-- Left side: Title and Subtitle -->
+			<div class="hero">
+				<div class="hero-text">
 					<h1 class="hero-title">Express Yourself with <span class="color-effect">Custom Tees</span></h1>
 					<p class="hero-subtitle">Unleash Your Style with Trendy, One-of-a-Kind Tees</p>
 					<div class="card flex justify-content-center">
@@ -17,7 +16,69 @@
 				<div class="hero-image">
 					<img src="../../images/t_shirt.jpeg" alt="Hero Image">
 				</div>
+			</div>
+		</div>
+		<div class="main-two">
+			<div class="hero">
+				<div class="hero-container">
 
+					<!-- Left side: Title and Subtitle -->
+					<div class="hero-content">
+						<h1 class="hero-title">Express Yourself with <span class="color-effect">Custom Tees</span></h1>
+						<p class="hero-subtitle">Unleash Your Style with Trendy, One-of-a-Kind Tees</p>
+						<div class="card flex justify-content-center">
+							<Button label="Explore Now" class="hero-button" />
+						</div>
+					</div>
+
+					<!-- Right side: Hero Image -->
+					<div class="hero-image">
+						<img src="../../images/t_shirt.jpeg" alt="Hero Image">
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="main-three">
+			<div class="hero">
+				<div class="hero-container">
+
+					<!-- Left side: Title and Subtitle -->
+					<div class="hero-content">
+						<h1 class="hero-title">Express Yourself with <span class="color-effect">Custom Tees</span></h1>
+						<p class="hero-subtitle">Unleash Your Style with Trendy, One-of-a-Kind Tees</p>
+						<div class="card flex justify-content-center">
+							<Button label="Explore Now" class="hero-button" />
+						</div>
+					</div>
+
+					<!-- Right side: Hero Image -->
+					<div class="hero-image">
+						<img src="../../images/t_shirt.jpeg" alt="Hero Image">
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="main-two">
+			<div class="hero">
+				<div class="hero-container">
+
+					<!-- Left side: Title and Subtitle -->
+					<div class="hero-content">
+						<h1 class="hero-title">Express Yourself with <span class="color-effect">Custom Tees</span></h1>
+						<p class="hero-subtitle">Unleash Your Style with Trendy, One-of-a-Kind Tees</p>
+						<div class="card flex justify-content-center">
+							<Button label="Explore Now" class="hero-button" />
+						</div>
+					</div>
+
+					<!-- Right side: Hero Image -->
+					<div class="hero-image">
+						<img src="../../images/t_shirt.jpeg" alt="Hero Image">
+					</div>
+
+				</div>
 			</div>
 		</div>
 	</div>
