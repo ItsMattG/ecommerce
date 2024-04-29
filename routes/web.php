4c8d4182895a6a3ce551ec321075af5c3f2fc8fb
ItsMattG <gleesonmatt96@gmail.com>
2024-04-29 20:39:24 +1000

    Formatting

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Inertia('Home');
+	return Inertia('Home');
 });
 
 Route::get('/women', function () {
-    return Inertia('Women');
+	return Inertia('Women');
 });
