b2ad27364e2b5ae3c24021a9857f875cf29edbf3
ItsMattG <gleesonmatt96@gmail.com>
2024-04-29 20:32:38 +1000

    Formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,14 @@ import 'primeicons/primeicons.css'
 
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
-  },
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-	  .use(PrimeVue)
-      .mount('body')
-  },
+	resolve: name => {
+		const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+		return pages[`./Pages/${name}.vue`]
+	},
+	setup({ App, props, plugin }) {
+		createApp({ render: () => h(App, props) })
+			.use(plugin)
+			.use(PrimeVue)
+			.mount('body')
+	},
 })
