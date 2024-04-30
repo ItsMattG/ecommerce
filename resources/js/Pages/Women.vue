1bf28ba189a218834b2c0443d5464790332ff6b1
ItsMattG <gleesonmatt96@gmail.com>
2024-04-30 20:09:50 +1000

    WIP: Layout on womens page.

--- resources/js/Pages/Women.vue
+++ resources/js/Pages/Women.vue
@@ -1,11 +1,21 @@
 <template>
-	<div>
-		women
-	</div>
+		<navbar></navbar>
+		<div style="background-color: red; height: 100%; width: 100%; display: flex;">
+			<div style="margin-top: 4rem; background-color: blue; display: flex; height: 30%; width: 100%; justify-content: center; align-items: center;">
+				<div style="background-color: green; display: flex; height: 100%; width: 80%;"></div>
+			</div>
+		</div>
+		<!-- Top -> Promo Bar -->
+		<!-- Left -> Filter -->
+		<!-- Right -> Grid layout of items -->
 </template>
 
 <script>
+import Navbar from '../Components/Navbar.vue';
 export default {
 	name: 'Women',
+	components: {
+		Navbar
+	}
 }
 </script>
